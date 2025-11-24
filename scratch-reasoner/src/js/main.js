972f78d334e01855b77b26feabcb2e31c1d7215c
beretta-wei <a0985616640@gmail.com>
2025-11-25 00:27:43 +0800

    Delete scratch-reasoner directory

--- scratch-reasoner/src/js/main.js
+++
@@ -1,9 +0,0 @@
-import { initControls } from "./ui/controlsView.js";
-import { initGrid } from "./ui/gridView.js";
-import { initStats } from "./ui/statsView.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-  initControls();
-  initGrid();
-  initStats();
-});
