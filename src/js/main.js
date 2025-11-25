cd86dc89240bd94e0f6a90b9fdaacc9174a34ffe
beretta-wei <a0985616640@gmail.com>
2025-11-25 13:38:28 +0800

    Reformat imports and event listener setup

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 import { initControls } from "./ui/controlsView.js";
 import { initGrid } from "./ui/gridView.js";
-import { initStats } from "./statsView.js";
+import { initStats } from "./ui/statsView.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   initControls();
