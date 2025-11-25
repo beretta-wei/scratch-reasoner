d730085c0e6e1ae776d9ab56291db725fc0c578c
beretta-wei <a0985616640@gmail.com>
2025-11-25 23:15:52 +0800

    Delete scratch-reasoner-fixed-controls-from-user directory

--- scratch-reasoner-fixed-controls-from-user/src/js/utils/dom.js
+++
@@ -1,8 +0,0 @@
-export const $ = (sel, root = document) => root.querySelector(sel);
-
-export function createElement(tag, className, text) {
-  const el = document.createElement(tag);
-  if (className) el.className = className;
-  if (text !== undefined) el.textContent = text;
-  return el;
-}
